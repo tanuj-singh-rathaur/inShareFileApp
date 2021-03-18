bd693263b96febe78f8b5bb73107d421896c2831
tanuj <tanujsingh635@gmail.com>
2021-03-19 00:04:14 +0530

    configured mongodb connection

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,17 +2,12 @@ const mongoose = require('mongoose')
 
 
 const mongodbConnection = () => {
-    mongoose.connect(process.env.DATABASE, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
-    })
-    const connection = mongoose.connection
+    mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true });
+    const connection = mongoose.connection;
     connection.once('open', () => {
-        console.log("database connection successful !")
+        console.log('Database connected 🥳🥳🥳🥳');
     }).catch(err => {
-        console.log("error during connecting !\n" + err)
-    })
+        console.log('Connection failed ☹️☹️☹️☹️');
+    });
 }
 module.exports = mongodbConnection
