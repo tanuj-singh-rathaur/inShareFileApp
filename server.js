cfa97c4a7197d422d7e189703698aa5ebca5f16e
tanuj <tanujsingh635@gmail.com>
2021-03-20 17:21:21 +0530

    added header origin for every host

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3000
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
