cda9ff531e8db3ef1149d297c6a39ce1628f807c
tanuj <tanujsingh635@gmail.com>
2021-03-20 17:38:57 +0530

    added heroku app url in header origin

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3000
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://insharefileapp.herokuapp.com/');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
