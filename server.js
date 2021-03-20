d16bf4a74fd0acbc617cb60e3a071a22164477fc
tanuj <tanujsingh635@gmail.com>
2021-03-20 17:08:48 +0530

    cors setup

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ const port = process.env.PORT || 3000
 
 
 
+app.use((req, res, next) => {
+
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+    req.headers("Access-Control-Allow-Methods", 'PUT,POST,PATCH,DELETE,GET')
+    if (req.method === 'OPTIONS') {
+
+        return res.status(200).json({})
+    }
+})
 
 app.use(express.json())
 app.use(express.static('public'))
@@ -25,16 +35,7 @@ app.use('/files', downlodsRouter)
 
 app.get('/', (req, res) => res.render('index'))
 
-app.use((req, res, next) => {
 
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-        "Access-Control-Allow-Headers", "*")
-    if (req.method === 'OPTIONS') {
-        req.header("Access-Control-Allow-Methods", 'PUT,POST,PATCH,DELETE,GET')
-        return res.status(200).json({})
-    }
-})
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server is active and running on port ${port}`)
