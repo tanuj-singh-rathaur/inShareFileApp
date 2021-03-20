2f9791b5e8ed721cd1c1a6a5329071ae9a2b6dc8
tanuj <tanujsingh635@gmail.com>
2021-03-20 12:13:57 +0530

    added some cors settings in server.js

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ const port = process.env.PORT || 3000
 
 
 // Cors 
-app.use(cors())
+
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(express.static('public'))
