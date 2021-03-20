d939305c81cded211a8a092398a62b9547692e35
tanuj <tanujsingh635@gmail.com>
2021-03-20 13:46:34 +0530

    setup for cors

--- server.js
+++ server.js
@@ -11,14 +11,7 @@ const downloadRouter = require('./routes/download')
 const port = process.env.PORT || 3000
 
 
-// Cors 
 
-const corsOptions = {
-    origin: process.env.ALLOWED_CLIENTS.split(',')
-    // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
-
-app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(express.static('public'))
@@ -35,6 +28,14 @@ app.listen(port, '0.0.0.0', () => {
     console.log(`Server is active and running on port ${port}`)
 
 })
+// Cors 
+
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
+console.log(corsOptions)
+app.use(cors(corsOptions))
 dbConnection()
 
 
