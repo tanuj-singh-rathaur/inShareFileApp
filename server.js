52cf6210011ad0ded536c0c4d2926f7ca0a3de85
tanuj <tanujsingh635@gmail.com>
2021-03-20 17:13:50 +0530

    again added cors dependency

--- server.js
+++ server.js
@@ -12,16 +12,7 @@ const port = process.env.PORT || 3000
 
 
 
-app.use((req, res, next) => {
-
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "*")
-    req.headers("Access-Control-Allow-Methods", 'PUT,POST,PATCH,DELETE,GET')
-    if (req.method === 'OPTIONS') {
-
-        return res.status(200).json({})
-    }
-})
+app.use(cors())
 
 app.use(express.json())
 app.use(express.static('public'))
